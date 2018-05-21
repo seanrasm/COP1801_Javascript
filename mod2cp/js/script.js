5a78f65c4be03f6b76ef837efc91bfffd33eaec1
seanrasm <35642475+seanrasm@users.noreply.github.com>
2018-05-20 22:29:30 -0500

    Added console log comments

--- mod2cp/js/script.js
+++ mod2cp/js/script.js
@@ -8,27 +8,39 @@ function wake() {
 
 //sum
 function sum_compute() {
+	console.log("Begin function \'sum\'");
 	var int_one = 3;
-    var int_two = 14;
+	console.log("int_one = " + int_one);
+	var int_two = 14;
+	console.log("int_two = " + int_two);
 	var result = int_one + int_two;
+	console.log("The sum is = " + result);
 	document.getElementById('sum').innerHTML = "Sum of two integers = " + result;
 }
 
 sum_compute();
 
 function sum_mult() {
+	console.log("Begin function \'multiply\'");
 	var int_one = 3;
-    var int_two = 14;
+	console.log("int_one = " + int_one);
+	var int_two = 14;
+	console.log("int_two = " + int_two);
 	var result = int_one * int_two;
+	console.log("The product is = " + result);
 	document.getElementById('mult').innerHTML = "Product of two integers = " + result;
 }
 
 sum_mult();
 
 function sum_trip() {
+	console.log("Begin function \'triple the sum\'");
 	var int_one = 3;
-    var int_two = 14;
+	console.log("int_one = " + int_one);
+	var int_two = 14;
+	console.log("int_two = " + int_two);
 	var result = (int_one + int_two) * 3;
+	console.log("Triple the sum is = " + result);
 	document.getElementById('trip').innerHTML = "Triple the sum of two integers = " + result;
 }
 
